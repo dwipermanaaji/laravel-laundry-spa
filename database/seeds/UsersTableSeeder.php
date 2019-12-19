5273787d8f90005811727902fb049417026b8526
Dwi <dwipermanaaji@gmail.com>
2019-12-19 11:25:34 +0700

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use app\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+            'role' => 0
+        ]);
+    }
+}
